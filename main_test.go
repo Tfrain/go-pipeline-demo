c8ae1d0721223513c84a8d0a83713e5fad7bf787
Tfrain <weichengdouble@outlook.com>
2021-04-22 22:24:00 +0800

    fix invalid workflow file

--- main_test.go
+++ main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 func TestHello(t *testing.T) {
- want := "Hello World!"
+ want := "Hello World"
  got := hello()
  if want != got {
   t.Fatalf("want %s, got %s\n", want, got)
